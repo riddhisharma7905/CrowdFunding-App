1c5ff43f178e529611daf7f0095ec131aa6b595d
riddhisharma7905 <riddhisharma7905@gmail.com>
2026-01-31 23:50:58 +0530

    Header

---
+++ app/components/Header.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-white backdrop-blur">
+    <div className="flex h-16 w-full items-center px-6">
+
+        <Link href="/" className="flex items-center gap-3">
+          <Image
+            src="/logo.png"
+            alt="BackIt Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <span className="text-2xl font-extrabold text-black tracking-tight">
+            BackIt
+          </span>
+        </Link>
+
+        <div className="flex items-center gap-4 ml-auto">
+          <Link href="/dashboard">
+            <button className="text-sm font-medium text-gray-700 hover:text-black">
+              Dashboard
+            </button>
+          </Link>
+
+          <Link href="/login">
+            <button className="text-sm font-medium text-gray-700 hover:text-black">
+              Sign In
+            </button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
